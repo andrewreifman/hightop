ae7dcab7bfdf53f7c9ad31b9722e0e68f4cc7a73
Bilal Mechairia <me@bilalmechairia.com>
2013-08-04 01:14:40 -0400

    wizard

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,7 @@
 //= require jquery.vmap.world
 //= require fullcalendar.min
 //= require gcal
+//= require jquery.bootstrap.wizard.min
 //= require turbolinks
 //= require dataTables/jquery.dataTables
 //= require bootstrap.min
