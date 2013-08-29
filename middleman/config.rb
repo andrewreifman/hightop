58828928945b9bf90a49c24f82a492633289d49b
Don Pottinger <don.pottinger@gmail.com>
2013-08-29 11:14:47 -0400

    Removes line comments from compiled css. [#55906400]

--- middleman/config.rb
+++ middleman/config.rb
@@ -3,9 +3,10 @@
 ###
 
 # Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+compass_config do |config|
+  # config.output_style = :compact
+  config.sass_options = { line_comments: false }
+end
 
 ###
 # Page options, layouts, aliases and proxies
