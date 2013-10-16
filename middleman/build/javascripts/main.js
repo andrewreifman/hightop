9db7801d75535104e0f0238c52a29dcf7df9ce32
Andrew Reifman <andrewr23@gmail.com>
2013-10-16 15:28:12 -0400

    middleman build

--- middleman/build/javascripts/main.js
+++ middleman/build/javascripts/main.js
@@ -286,11 +286,7 @@
         $total = navigation.find("li").length;
         $current = index + 1;
         $percent = ($current / $total) * 100;
-        $("#wizard").find(".progress-bar").css("width", $percent + "%");
-        $(document).ready(function() {});
-        return $("#rootwizard").bootstrapWizard({
-          onNext: function(tab, navigation, index) {}
-        });
+        return $("#wizard").find(".progress-bar").css("width", $percent + "%");
       }
     });
     /*
